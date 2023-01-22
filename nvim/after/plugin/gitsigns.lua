14dd47f24de9c52185e569d6fd0d1e14f28fa3c1
david.ahmed <davedshepherd@gmail.com>
2023-01-22 23:10:41 +0100

    Lovin Gitsigns

---
+++ nvim/after/plugin/gitsigns.lua
@@ -0,0 +1,16 @@
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+  end
+}
+
