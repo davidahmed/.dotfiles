9e13802a7529b49eec3c1c9676823c4754b7fd3a
david.ahmed <davedshepherd@gmail.com>
2023-01-22 23:08:35 +0100

    Moved Packer installation list to a separate module

---
+++ nvim/lua/david/packer.lua
@@ -0,0 +1,60 @@
+require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+	use 'preservim/nerdtree' 
+
+	use 'ellisonleao/gruvbox.nvim'
+	use 'rktjmp/lush.nvim'
+
+	use 'tpope/vim-fugitive'
+	use 'tpope/vim-surround'
+
+	use 'neovim/nvim-lspconfig'
+	use 'nvim-treesitter/nvim-treesitter'
+	use 'nvim-treesitter/playground'
+
+	use 'nvim-lualine/lualine.nvim'
+	use 'kyazdani42/nvim-web-devicons'
+
+	-- Autocompletion --
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+
+	-- Snippets --
+	use 'saadparwaiz1/cmp_luasnip'
+	use 'L3MON4D3/LuaSnip'
+	use "rafamadriz/friendly-snippets"
+
+  -- vim-elxir --
+  use 'elixir-editors/vim-elixir'
+	-- Telescope --
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+  use {
+    'nvim-telescope/telescope-media-files.nvim',
+    requires = { {'nvim-telescope/telescope.nvim'} },
+    config = function() 
+      require('telescope').load_extension('media_files')
+    end
+  }
+	-- Formatter --
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("null-ls").setup()
+		end,
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+
+  use {
+  'lewis6991/gitsigns.nvim',
+  }
+
+  use('theprimeagen/harpoon')
+	if Packer_bootstrap then require('packer').sync() end
+end)
+
