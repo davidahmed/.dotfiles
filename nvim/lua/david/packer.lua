c4c37ab9781fdf8365cbb2c9eecbe3292283ec27
david.ahmed <davedshepherd@gmail.com>
2023-01-25 22:06:49 +0100

    Install LSP

--- nvim/lua/david/packer.lua
+++ nvim/lua/david/packer.lua
@@ -2,19 +2,22 @@ require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 	use 'preservim/nerdtree' 
 
-  use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-        require("rose-pine").setup()
-        vim.cmd('colorscheme rose-pine')
-    end
-})
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			require("rose-pine").setup()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
 
 	use 'tpope/vim-fugitive'
 	use 'tpope/vim-surround'
 
-	use 'neovim/nvim-lspconfig'
+
+	-- LSP
+	use 'neovim/nvim-lspconfig' --enable LSP
+	use 'williamboman/nvim-lsp-installer'
 	use 'nvim-treesitter/nvim-treesitter'
 	use 'nvim-treesitter/playground'
 
@@ -33,20 +36,21 @@ require('packer').startup(function()
 	use 'L3MON4D3/LuaSnip'
 	use "rafamadriz/friendly-snippets"
 
-  -- vim-elxir --
-  use 'elixir-editors/vim-elixir'
+	use('mbbill/undotree')
+	-- vim-elxir --
+	use 'elixir-editors/vim-elixir'
 	-- Telescope --
 	use {
 		'nvim-telescope/telescope.nvim',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-  use {
-    'nvim-telescope/telescope-media-files.nvim',
-    requires = { {'nvim-telescope/telescope.nvim'} },
-    config = function() 
-      require('telescope').load_extension('media_files')
-    end
-  }
+	use {
+		'nvim-telescope/telescope-media-files.nvim',
+		requires = { {'nvim-telescope/telescope.nvim'} },
+		config = function()
+			require('telescope').load_extension('media_files')
+		end
+	}
 	-- Formatter --
 	use({
 		"jose-elias-alvarez/null-ls.nvim",
@@ -56,11 +60,11 @@ require('packer').startup(function()
 		requires = { "nvim-lua/plenary.nvim" },
 	})
 
-  use {
-  'lewis6991/gitsigns.nvim',
-  }
+	use {
+		'lewis6991/gitsigns.nvim',
+	}
 
-  use('theprimeagen/harpoon')
+	use('theprimeagen/harpoon')
 	if Packer_bootstrap then require('packer').sync() end
 end)
 
