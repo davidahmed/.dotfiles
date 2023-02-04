aebfe2d0f1eae3f7462aea3346fa2a2b4d954ed3
david.ahmed <davedshepherd@gmail.com>
2023-02-05 00:28:02 +0100

    Migrate telescope configs into its own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,17 +13,15 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
 
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 --------- Visual Enhancements --------------------
-require('lualine').setup{}
-require'nvim-web-devicons'.setup {}
---------- Telescope  --------------------
-map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>")
-map('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-map('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>")
-map('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+require("lualine").setup({})
+require("nvim-web-devicons").setup({})
+
